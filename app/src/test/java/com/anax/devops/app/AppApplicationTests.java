4d456f6692c17de45cda6b2d0365cba65b2778ad
Andresm98 <andresm17u7@hotmail.com>
2026-05-26 20:46:50 -0500

    chore: initial project structure with maven, checkstyle and jacoco configurations

---
+++ app/src/test/java/com/anax/devops/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anax.devops.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
